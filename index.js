ee4564339110d3b5c9ea556efd3b7f2a06bf374e
Hugo Fitas <hfitas@gmail.com>
2021-06-03 09:58:31 +0100

    fixed dotenv import on index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-require("dotenv").config;
-const port = process.env.PORT || 5000;
+require("dotenv").config();
+const port = process.env.PORT;
 
 app.listen(port, (err) => {
   err ? console.log(err) : console.log(`App is running at port ${port}.`);
