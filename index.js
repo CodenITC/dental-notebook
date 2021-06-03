3f8f8abd12561c02bd213c95c33fe70fafd602b3
Hugo Fitas <hfitas@gmail.com>
2021-06-03 09:59:19 +0100

    fixed port on index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, (err) => {
   err ? console.log(err) : console.log(`App is running at port ${port}.`);
