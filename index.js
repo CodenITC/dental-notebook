f5d6f1c533497bbe5a24b13c8857937ef7eded8e
Hugo Fitas <hfitas@gmail.com>
2021-06-10 11:54:01 +0100

    added express router

--- index.js
+++ index.js
@@ -4,63 +4,32 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const connection = require("./config-db");
 
+const todosRouter = require("./routes/todos.route.js");
+const patientsRouter = require("./routes/patients.route.js");
+
 connection.connect((error) => {
   if (error) console.log(error);
   else console.log(`connected to database on thread ${connection.threadId}`);
 });
 
 app.use(express.json());
+app.use("/todos", todosRouter);
+app.use("/patients", patientsRouter);
 
-app.get("/todos", (req, res) => {
-  connection.query("SELECT * FROM todos", (error, results) => {
-    if (error) res.status(500).send(error);
-    else {
-      if (results.length) res.status(200).json(results);
-      else res.status(404).send("Todos not found.");
-    }
-  });
-});
+// Appointments START
 
-app.post("/todos", (req, res) => {
-  const newTodos = req.body;
-  connection.query("INSERT INTO todos SET ?", [newTodos], (error, results) => {
-    if (error) response.status(500).send(error);
-    else {
-      const newTodoId = results.insertId;
-      connection.query(
-        "SELECT * FROM todos WHERE id = ?",
-        [newTodoId],
-        (error, results) => {
-          if (error) res.status(500).send(error);
-          else res.status(200).json(results[0]);
-        }
-      );
-    }
-  });
-});
+// Appointments END
 
-app.delete("/todos/:id", (req, res) => {
-  const id = req.params.id;
-  connection.query("DELETE FROM todos WHERE id = ?", [id], (error, results) => {
-    if (error) res.status(500).send(error);
-    else res.send("The todo was successfully deleted");
-  });
-});
+/* 
+RETRIEVING INFO FROM APPOINTMENTS:
 
-// RETRIEVING INFO FROM TODOS:
+GET request
+http://localhost:5000/appointments?date=""
 
-// GET request (todos.todo_item)
-// http://localhost:5000/todos
 
-// ADD NEW INFO TO TODOS:
-
-// POST request
-// http://localhost:5000/todos
-
-// DELETE INFO FROM TODOS:
-
-// DELETE request
-// http://localhost:5000/todos/:id
+GET request by id
+http://localhost:5000/appointments/:id 
+*/
 
 app.listen(port, (err) => {
   err ? console.log(err) : console.log(`App is running at port ${port}.`);
---
+++ routes/patients.route.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const router = express.Router();
+const connection = require("../config-db");
+
+// GET /patients
+router.get("/", (req, res) => {
+  connection.query("SELECT * FROM patients", (error, results) => {
+    if (error) res.status(500).send(error);
+    else {
+      if (results.length) res.status(200).json(results);
+      else res.status(404).send("Patients not found.");
+    }
+  });
+});
+
+// GET /patients/:id
+router.get("/:id", (req, res) => {
+  const patientId = req.params.id;
+
+  connection.query(
+    "SELECT * FROM patients WHERE id = ?",
+    [patientId],
+    (error, results) => {
+      if (error) res.status(500).send(error);
+      else {
+        if (results.length) res.status(200).json(results);
+        else res.status(404).send(`Patient with id ${patientId} not found.`);
+      }
+    }
+  );
+});
+
+// POST /patients
+router.post("/", (req, res) => {
+  const newPatient = req.body;
+  connection.query(
+    "INSERT INTO patients SET ?",
+    [newPatient],
+    (error, results) => {
+      if (error) res.status(500).send(error);
+      else {
+        const newPatientId = results.insertId;
+        connection.query(
+          "SELECT * FROM patients WHERE id=?",
+          [newPatientId],
+          (error, results) => {
+            if (error) res.status(500).send(error);
+            else res.status(200).json(results[0]);
+          }
+        );
+      }
+    }
+  );
+});
+
+// PUT /patients/:id
+router.put("/:id", (req, res) => {
+  const patientId = req.params.id;
+  connection.query(
+    "SELECT * FROM patients WHERE id = ?",
+    [patientId],
+    (error, results) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        const patientFromDb = results[0];
+        if (patientFromDb) {
+          const updatedPatient = req.body;
+          connection.query(
+            "UPDATE patients SET ? WHERE id = ?",
+            [updatedPatient, patientId],
+            (error) => {
+              if (error) {
+                res.status(500).send(error);
+              } else {
+                const updatedPatientInfo = {
+                  ...patientFromDb,
+                  ...updatedPatient,
+                };
+                res.status(200).json(updatedPatientInfo);
+              }
+            }
+          );
+        } else {
+          res.status(404).send(`Patient with the id ${patientId} not found`);
+        }
+      }
+    }
+  );
+});
+
+module.exports = router;
